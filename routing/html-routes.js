e6ff0fe66fc63c1b0a8bf82f0d4cc4d8be1d2e51
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-08-03 14:22:33 -0400

    photos and article summary scraping

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,9 +1,13 @@
 module.exports = function(app, request, cheerio, db) {
 
 
+	//This will get the homepage upon start up of the website
     app.get('/', function(req, res) {
 
-      res.render('home', {})
+      	db.article.find({}, function(err, data) {
+		if (err) throw err;
+		res.render("home", {article: data});
+		})
                
     });
 
@@ -16,20 +20,26 @@ module.exports = function(app, request, cheerio, db) {
   
 
 	  //the title and link scrapped from Orlando News
-	    $('h3.postTitle').each(function(i, element){
+	    $('.blogPost').each(function(i, element){
 
 	      var result = {};
 
 	      //this will insert the article title
-	      var title = $(this).text();
+	      var title = $(this).find('h3.postTitle').text();
 	      //this will insert the article title
-	      var link = $(this).find('a').attr('href');
+	      var link = $(this).find('h3.postTitle').find('a').attr('href');
+		  //this will insert the article title
+	      var summ = $(this).find('.postSummary').text();
+	      //this will insert the article title
+	      var photo = $(this).find('.storyTeaser').find('img').attr('src');
 
 	      //if there the title is not empty or blank
 	      if (title !== '') {
           
           result.Title= title;
           result.Link= link;
+          result.Summary = summ;
+          result.Photo= photo;
 
           //this will save the pulled data in the article collection
           db.article.save(result, function (err, docs) {
